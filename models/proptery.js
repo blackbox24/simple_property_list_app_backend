2b869360aae82ac5bb914034cd1e16808ceb9141
blackbox24 <nicardid@gmail.com>
2025-05-11 16:27:17 +0000

    create a property model file and implementation

---
+++ models/proptery.js
@@ -0,0 +1,38 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../config/dbconf');
+
+const Property = sequelize.define('Property', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'properties'
+});
+
+module.exports = Property;
