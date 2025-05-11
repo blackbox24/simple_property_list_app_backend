ad4d821d726e5925edba5c56b1e11746b714547a
blackbox24 <nicardid@gmail.com>
2025-05-11 16:28:31 +0000

    chore: added cors packages

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const sequelize = require("./config/dbconf");
+
+// Define a simple route
+// app.get("/", (req, res) => {
+//     res.json({ message: "Welcome to the application." });    
+// });
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
