af4b32408c34e65fc9cd901474a85637ddf7e3b7
blackbox24 <nicardid@gmail.com>
2025-05-13 15:32:02 +0000

    modified cors to aceept all

--- index.js
+++ index.js
@@ -7,18 +7,7 @@ const PropertyRoutes = require("./routers/PropteryRoutes.js");
 const cors = require("cors");
 
 // Middleware
-// CORS configuration
-app.use(
-  cors({
-    origin: [
-      'http://localhost:5500',
-      'http://127.0.0.1:5500',
-      'https://simple-property-list-frontend.vercel.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
