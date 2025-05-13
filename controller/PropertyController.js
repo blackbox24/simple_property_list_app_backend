b4cfa1ed5b33ca12eb905494fd1aaa9f7eef3fc9
blackbox24 <nicardid@gmail.com>
2025-05-13 10:17:20 +0000

    update properties api

--- controller/PropertyController.js
+++ controller/PropertyController.js
@@ -1,5 +1,13 @@
 const sequelize = require('../config/dbconf');
 const Property = require('../models/proptery');
+const multer = require("multer");
+const {validateProperty,validatePropertyImage} = require('../validations/propertyValidator');
+const upload = multer({ dest: 'uploads/' });
+const fs = require('fs');
+const path = require('path');
+const { Op } = require('sequelize');
+
+
 
 const PropertyController = {
     async getAllProperties(req,rep){
@@ -13,40 +21,25 @@ const PropertyController = {
     },
     async createProperty(req,rep){
         try{
+            is_validate = validateProperty(req.body);
+            is_validate_image = validatePropertyImage(req.file);
+
+            // Validate the request body
+            if(!is_validate.isValid){
+                return rep.status(400).send({error:is_validate.errors});
+            }   
+
+            if(!is_validate_image.isValid){
+                return rep.status(400).send({error:is_validate_image.errors});
+            }
             const property = await Property.create(req.body);
+
             return rep.status(201).send(property);
         }catch(err){
             console.error(err);
             return rep.status(500).send({error:"Internal Server Error"});
         }
     },
-    async updateProperty(req,rep){
-        try{
-            const property = await Property.update(req.body,{
-                where:{
-                    id:req.params.id
-                }
-            });
-            return rep.status(200).send(property);
-        }catch(err){
-            console.error(err);
-            return rep.status(500).send({error:"Internal Server Error"});
-        }
-    },
-    async deleteProperty(req,rep){
-        try{
-            const property = await Property.destroy({
-                where:{
-                    id:req.params.id
-                }
-            });
-            return rep.status(200).send(property);
-        }catch(err){
-            console.error(err);
-            return rep.status(500).send({error:"Internal Server Error"});
-        }
-    },
-
     async getPropertyById(req,rep){
         try{
             const property = await Property.findByPk(req.params.id);
--- seeders/20250512193430-property_seed.js
+++ seeders/20250512193430-property_seed.js
@@ -7,7 +7,7 @@ module.exports = {
       name: 'Property 1',
       description: 'Description for Property 1',
       price: 100000,
-      imageUrl: 'https://example.com/property1.jpg',
+      image_url: 'https://example.com/property1.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -15,7 +15,7 @@ module.exports = {
       name: 'Property 2',
       description: 'Description for Property 2',
       price: 100000,
-      imageUrl: 'https://example.com/property3.jpg',
+      image_url: 'https://example.com/property3.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
@@ -23,7 +23,7 @@ module.exports = {
       name: 'Property 3',
       description: 'Description for Property 3',
       price: 100000,
-      imageUrl: 'https://example.com/property2.jpg',
+      image_url: 'https://example.com/property2.jpg',
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
